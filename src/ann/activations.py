fa7dfc9e72c4071ec4a734c7d7fb555487ec232e
anan madhav <126330623+ananmadhav@users.noreply.github.com>
2026-03-01 08:50:07 +0530

    Add activation functions ReLU, Sigmoid, Tanh, Softmax
    Implemented ReLU, Sigmoid, Tanh, and Softmax activation functions with their forward and backward methods.

--- src/ann/activations.py
+++ src/ann/activations.py
@@ -1,4 +1,49 @@
-"""
-Activation Functions and Their Derivatives
-Implements: ReLU, Sigmoid, Tanh, Softmax
-"""
+import numpy as np
+
+class ReLU:
+    def __init__(self):
+        self.X = None
+
+    def forward(self, X):
+        self.X = X
+        return np.maximum(0, X)
+
+    def backward(self, d_out):
+        dX = d_out.copy()
+        dX[self.X <= 0] = 0
+        return dX
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, X):
+        self.out = 1 / (1 + np.exp(-X))
+        return self.out
+
+    def backward(self, d_out):
+        return d_out * self.out * (1 - self.out)
+
+class Tanh:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, X):
+        self.out = np.tanh(X)
+        return self.out
+
+    def backward(self, d_out):
+        return d_out * (1 - self.out ** 2)
+
+class Softmax:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, X):
+        X_shifted = X - np.max(X, axis=1, keepdims=True)
+        exp_X = np.exp(X_shifted)
+        self.out = exp_X / np.sum(exp_X, axis=1, keepdims=True)
+        return self.out
+
+    def backward(self, d_out):
+        return d_out
